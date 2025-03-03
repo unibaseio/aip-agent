76791c8524aedf9cff20cf77690c24ea306868b6
mossv2 <mossv2@github.com>
2025-03-03 10:31:42 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aip_agent",  # 替换为你的包名
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    
+    # 项目元数据
+    author="felix",
+    author_email="felix@unibase.io",
+    description="AIP Agent for Unibase, include agent interraction, chain operation, agent tool management, etc.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/unibaseio/aip_agent",
+    
+    # 项目依赖
+    install_requires=[
+        "mcp>=1.2.1",
+        "chromadb>=0.6.0",
+        "web3>=7.8.0",
+        "twikit>=2.3.3",
+        "fastapi>=0.115.6",
+        "instructor>=1.7.0",
+        "opentelemetry-distro>=0.50b0",
+        "opentelemetry-exporter-otlp-proto-http>=1.29.0",
+        "pydantic-settings>=2.7.0",
+        "pydantic>=2.10.4",
+        "pyyaml>=6.0.2",
+        "rich>=13.9.4",
+        "typer>=0.15.1",
+        "numpy>=2.2.1",
+        "scikit-learn>=1.6.0",
+        "gradio>=5.16.0",
+        "loguru>=0.7.3",
+    ],
+    
+    # 可选：指定 Python 版本要求
+    python_requires=">=3.10",
+) 
